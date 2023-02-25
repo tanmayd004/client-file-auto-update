6a25e41ee4597e3a40b4f9fa8a780751cca1617f
tanmayd004 <72381262+tanmayd004@users.noreply.github.com>
2023-02-26 01:20:42 +0530

    Update client.py

--- client.py
+++ client.py
@@ -16,7 +16,7 @@ filename = "client.py"
 
 # GitHub API endpoint for getting the latest commit timestamp
 
-endpoint = f"https://api.github.com/tanmayd004/client-file-auto-update/commits?path=client.py&per_page=1"
+endpoint = f"https://api.github.com/repos/tanmayd004/client-file-auto-update/commits?path=client.py&per_page=1"
 
 def check_for_updates():
 
